9d62468b8628c08adbb3cef3b9d24d750bdb85be
manjeetshrestha <62466213+manjeetshrestha@users.noreply.github.com>
2021-09-23 12:43:39 +0545

    Completed version

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const btnHamburger = document.querySelector('#btnHamburger')
+const header = document.querySelector('#header')
+const modal = document.querySelector('#menu_cont')
+const body = document.querySelector('body')
+btnHamburger.addEventListener('click',function(){
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open');
+        header.classList.remove('open');
+        modal.classList.add('hidden');
+        body.classList.remove('scroll-lock');
+    }
+    else{
+        btnHamburger.classList.add('open');
+        header.classList.add('open');
+        modal.classList.remove('hidden');
+        body.classList.add('scroll-lock');
+    }
+})
